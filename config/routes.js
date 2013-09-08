de45b97768ccbebbb179ede1cdd621fee78aff24
ethan <waye929@gmail.com>
2013-09-08 11:51:51 -0400

    Added routes for league controller.

--- config/routes.js
+++ config/routes.js
@@ -15,6 +15,15 @@ module.exports = function (app, passport, auth) {
   
   app.param('userId', users.user)
   
+  var leagues = require('../app/controllers/leagues')  
+  app.get('/leagues', leagues.all)
+  app.post('/leagues', auth.requiresLogin, leagues.create)
+  app.get('/leagues/:leagueId', leagues.show)
+  app.put('/leagues/:leagueId', auth.requiresLogin, leagues.update)
+  app.del('/leagues/:leagueId', auth.requiresLogin, leagues.destroy)
+ 
+  app.param('leagueId', leagues.league)
+  
   // home route
   var index = require('../app/controllers/index')
   app.get('/', index.render)
